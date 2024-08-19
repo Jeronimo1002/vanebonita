ec5897e241c9a9b2027cffbb50b23cff854ca7b9
Jeronimo1002 <eduardomanzano667@gmail.com>
2024-08-19 17:32:15 -0600

    Update poema.js

--- poema.js
+++ poema.js
@@ -9,12 +9,3 @@ document.getElementById('closeButton').addEventListener('click', function() {
     const overlay = document.getElementById('overlay');
     overlay.style.display = 'none';  // Oculta el overlay
 });
-
-
-
-document.getElementById('save-note').addEventListener('click', function() {
-    const noteContent = document.getElementById('note-text').value;
-    // Aquí puedes agregar el código para guardar la nota, por ejemplo, enviando el contenido a un servidor
-    // También podrías actualizar el contenido de alguna forma en la interfaz de usuario
-    alert('Nota guardada: ' + noteContent);
-});
