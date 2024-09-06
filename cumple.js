7475fd4ff0cac3cf5083048297532be6a3e18c9b
Jeronimo1002 <eduardomanzano667@gmail.com>
2024-09-06 00:28:01 -0600

    Actualizar cumple.js

--- cumple.js
+++ cumple.js
@@ -2,32 +2,6 @@ const targetDate = new Date('2024-09-06'); // Fecha del cumpleaños
         const today = new Date(); // Fecha actual
         const modal = document.getElementById('birthdayModal');
 
-        // Obtener el año y mes actuales para mostrar en el calendario (opcional)
-        const year = today.getFullYear();
-        const month = today.getMonth();
-        const monthElement = document.getElementById('month');
-        const calendar = document.getElementById('calendar');
-
-        // Nombres de los meses
-        const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-        monthElement.innerText = monthNames[month] + " " + year;
-
-        // Obtener el número de días del mes actual y el primer día del mes
-        const daysInMonth = new Date(year, month + 1, 0).getDate();
-        const firstDayOfMonth = new Date(year, month, 1).getDay();
-
-        // Añadir celdas vacías antes del primer día del mes
-        for (let i = 0; i < firstDayOfMonth; i++) {
-            calendar.innerHTML += '<div></div>';
-        }
-
-        // Añadir los días del mes al calendario
-        for (let day = 1; day <= daysInMonth; day++) {
-            const currentDay = new Date(year, month, day);
-            const className = currentDay < today ? 'past' : (currentDay.toDateString() === today.toDateString() ? 'today' : 'future');
-            calendar.innerHTML += `<div class="${className}">${day}</div>`;
-        }
-
         // Función para mostrar el modal
         function showModal() {
             modal.style.display = 'flex'; // Muestra el modal
